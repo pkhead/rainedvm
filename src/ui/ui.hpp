68a688c8dc170a77509793bf4e862a2235e31173
pkhead <chromosoze@gmail.com>
2025-08-26 15:40:22 -0500

    1. improve about window 2. minor ui code reorganization

---
+++ src/ui/ui.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#   include <wx/wx.h>
+#endif
+#include <wx/wxhtml.h>
+
+#include "../vm/vm.hpp"
+
+constexpr int ITEM_SPACING = 2;
+
+enum {
+    ID_VersionList = wxID_HIGHEST,
+    ID_InstallButton,
+
+    ID_FetchCheckTimer,
+};
+
+class OverriddenHtmlWindow : public wxHtmlWindow {
+public:
+	OverriddenHtmlWindow(
+        wxWindow *parent, wxWindowID id = -1,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        long style = wxHW_SCROLLBAR_AUTO,
+        const wxString& name = _T("htmlWindow"));
+    
+	void OnLinkClicked(const wxHtmlLinkInfo& link);
+};
+
+OverriddenHtmlWindow::OverriddenHtmlWindow(
+    wxWindow *parent, wxWindowID id, const wxPoint& pos, const wxSize& size,
+    long style, const wxString& name
+) : wxHtmlWindow(parent, id, pos, size, style, name) {}
+
+void OverriddenHtmlWindow::OnLinkClicked(const wxHtmlLinkInfo& link) {
+    wxLaunchDefaultBrowser(link.GetHref());
+}
+
+class RainedVMApp : public wxApp {
+public:
+    virtual bool OnInit();
+};
+
+class RainedVMFrame : public wxFrame {
+public:
+    RainedVMFrame();
+
+private:
+    std::atomic_bool is_vm_fetch_done = false;
+    VersionManager vm;
+    int activeVersionIndex = -1;
+    int selectedVersionIndex = -1;
+
+    wxButton *installButton;
+    wxListBox *versionListBox;
+    wxStaticText *versionLabel;
+    wxHtmlWindow *htmlWindow;
+
+    wxPanel *mainPanel = nullptr;
+    wxTimer *timer = nullptr;
+    std::unique_ptr<std::thread> thread;
+
+    void Fetch();
+
+    void ClearPage();
+    void ConstructFetchPage();
+    void ConstructVersionSelector();
+
+    void OnVersionSelect(wxCommandEvent &event);
+    void OnExit(wxCommandEvent &event);
+    void OnAbout(wxCommandEvent &event);
+    void OnInstallButtonPressed(wxCommandEvent &event);
+    void OnFetchCheckTimer(wxTimerEvent &event);
+};
+
+class AboutDialog : public wxDialog {
+public:
+    AboutDialog(wxWindow *parent, wxWindowID id);
+};
